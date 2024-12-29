23a3d5256d60f8e1946af9c450a113828c68a459
Azhar-0401 <azharansari.surajpur@gmail.com>
2024-12-29 19:32:13 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -3,14 +3,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res)=>{
-    res.send("Hello")
-});
+app.get("/user", (req, res)=>{
+    res.send({firstName: "Azhar", lastName : "Ansari"});
+})
 
 
 app.use("/test", (req, res)=>{
     res.send("Hello from the server")
 });
+
+// app.use("/", (req, res)=>{
+//     res.send("Hello")
+// });
+
+app.post("/user", (req, res)=>{
+    // console.log("save data to database"); 
+    res.send("data successfully save to the database");
+})
+
+app.delete("/user", (req, res)=>{
+    // console.log("save data to database"); 
+    res.send("Deleted successfully");
+})
+
+
+app.use("/user", (req, res)=>{
+    res.send("HAHAHA");
+})
+
 
 app.listen(3000, ()=>{
     console.log("server is successfully");
