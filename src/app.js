d61b7d72bcdee3d452681141b9ccafcb950b2e53
Azhar-0401 <azharansari.surajpur@gmail.com>
2024-12-30 14:39:23 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,37 +1,56 @@
-// console.log("this is starting file")
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlwares/auth");
 
 const app = express();
 
-app.get("/user", (req, res)=>{
-    res.send({firstName: "Azhar", lastName : "Ansari"});
-})
+// Middleware for admin routes
+app.use("/admin", adminAuth);
 
+// User route handling
+app.get(
+    "/user",
+    (req, res, next) => {
+        console.log("Handling the route user!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 2!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 3!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 4!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 5!!");
+        res.send("5th Response!!");
+    }
+);
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from the server")
+// Login route
+app.post("/user/login", (req, res) => {
+    res.send("User logged in successfully!");
 });
 
-// app.use("/", (req, res)=>{
-//     res.send("Hello")
-// });
+// User data route with authentication
+app.get("/user/data", userAuth, (req, res) => {
+    res.send("User Data Sent");
+});
 
-app.post("/user", (req, res)=>{
-    // console.log("save data to database"); 
-    res.send("data successfully save to the database");
-})
+// Admin routes
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All Data Sent");
+});
 
-app.delete("/user", (req, res)=>{
-    // console.log("save data to database"); 
-    res.send("Deleted successfully");
-})
+app.get("/admin/deleteUser", (req, res) => {
+    res.send("Deleted a user");
+});
 
-
-app.use("/user", (req, res)=>{
-    res.send("HAHAHA");
-})
-
-
-app.listen(3000, ()=>{
-    console.log("server is successfully");
+// Start server
+app.listen(7777, () => {
+    console.log("Server is successfully listening on port 7777...");
 });
